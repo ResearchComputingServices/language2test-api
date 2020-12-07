ef7eb52e75252a62e9448abba6a5370600db1f51
Kevin <kevin@macintosh-5.carleton.ca>
2020-12-07 14:23:15 -0500

    Add Test Assignation export

--- language2test_api/web/test_assignation_view.py
+++ language2test_api/web/test_assignation_view.py
@@ -1,6 +1,6 @@
 from language2test_api.extensions import oidc
 from flask import request
-from flask import json, jsonify, Response
+from flask import json, jsonify, Response, send_file
 from language2test_api.extensions import db, ma
 from language2test_api.web.common_view import language2test_bp
 from language2test_api.decorators.crossorigin import crossdomain
@@ -9,7 +9,8 @@ from language2test_api.providers.test_assignation_provider import TestAssignatio
 from language2test_api.models.test_assignation import TestAssignation, TestAssignationSchema
 from language2test_api.providers.user_provider import UserProvider
 from language2test_api.models.user import User
-
+from io import BytesIO
+import pandas as pd
 provider = TestAssignationProvider()
 user_provider = UserProvider()
 test_assignation_schema = TestAssignationSchema(many=False)
@@ -126,11 +127,87 @@ def get_instructor_test_assignation():
 
     return response
 
+@language2test_bp.route("/test_assignation/export", methods=['GET'])
+@crossdomain(origin='*')
+@authentication
+def export_test_assignation():
+    specific_id = request.args.get('id')
+    if specific_id is None:
+        try:
+            records = []
+            assignations = TestAssignation.query.all()
+            for a in assignations:
+                student_class_list = []
+                for s_c in a.student_class:
+                    student_class_list.append(s_c.display)
+                records.append({
+                    "Id": a.id,
+                    "Test": a.test.name,
+                    "Student Class": ", ".join(student_class_list),
+                    "Start Date": a.start_datetime,
+                    "End Date": a.end_datetime
+                    })
 
+            output = BytesIO()
+            with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
+                pd.DataFrame(records).to_excel(writer,
+                                               sheet_name="{} summary".format('test_assignation'),
+                                               index=False)
+                workbook = writer.book
+                worksheet = writer.sheets["{} summary".format('test_assignation')]
+                format = workbook.add_format()
+                format.set_align('center')
+                format.set_align('vcenter')
+                worksheet.set_column('A:A', 15, format)
+                worksheet.set_column('B:E', 30, format)
+                writer.save()
+            output.seek(0)
+            return send_file(output,
+                             attachment_filename="Test Assignation Summary" + '.xlsx',
+                             mimetype="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                             as_attachment=True, cache_timeout=-1)
+        except Exception as e:
+            error = {"exception": str(e), "message": "Exception has occurred. Check the format of the request."}
+            response = Response(json.dumps(error), 404, mimetype="application/json")
+            return response
 
+    if specific_id is not None:
+        try:
+            assignations = TestAssignation.query.all()
+            records = []
+            for session in assignations:
+                if session.id == int(specific_id):
+                    specific_session = session
+            student_class_list = []
+            for s_c in specific_session.student_class:
+                student_class_list.append(s_c.display)
+            records.append({
+                "Id": specific_session.id,
+                "Test": specific_session.test.name,
+                "Student Class": ", ".join(student_class_list),
+                "Start Date": specific_session.start_datetime,
+                "End Date": specific_session.end_datetime
+            })
 
-
-
-
-
-
+            output = BytesIO()
+            with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
+                pd.DataFrame(records).to_excel(writer,
+                                               sheet_name="{} summary".format('test_assignation'),
+                                               index=False)
+                workbook = writer.book
+                worksheet = writer.sheets["{} summary".format('test_assignation')]
+                format = workbook.add_format()
+                format.set_align('center')
+                format.set_align('vcenter')
+                worksheet.set_column('A:A', 15, format)
+                worksheet.set_column('B:E', 30, format)
+                writer.save()
+            output.seek(0)
+            return send_file(output,
+                             attachment_filename="Test Assignation Details" + '.xlsx',
+                             mimetype="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                             as_attachment=True, cache_timeout=-1)
+        except Exception as e:
+            error = {"exception": str(e), "message": "Exception has occurred. Check the format of the request."}
+            response = Response(json.dumps(error), 404, mimetype="application/json")
+            return response
